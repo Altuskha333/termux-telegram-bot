f0bd6b6c893fdb82333170047bdd6827ebb3ac7e
Altuskha333 <136063242+Altuskha333@users.noreply.github.com>
2023-06-09 13:48:57 +0500

    Отвечает за всё

--- bot.py
+++ bot.py
@@ -1,77 +1,30 @@
-импортировать   случайный
-
- время  импорта
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from opengpt import OpenGPT
 
-# функция для обработки команды /start
+# Создаем экземпляр OpenGPT
+forefront = OpenGPT(provider="forefront", type="completion", options={...})
 
+# Функция-обработчик команды /start
 def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот, который может помочь вам с ответами на ваши вопросы.")
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте, мне надо выдать права администрации чтобы я могла читать сообщения и анализировать их")
-
-# функция для повторения сообщения
-
-def repeat_message(context):
-
-    messages = context.bot.get_chat(context.job.context).get('messages')
-
-    if messages:
-
-        message = random.choice(messages)
-
-        context.bot.send_message(chat_id=context.job.context, text=message)
-
-# функция для обработки сообщений
-
+# Функция-обработчик текстовых сообщений
 def echo(update, context):
+    # Получаем текст сообщения от пользователя
+    user_input = update.message.text
 
-    messages = context.bot.get_chat(update.effective_chat.id).get('messages', [])
+    # Генерируем ответ с помощью OpenGPT
+    response = forefront.generate(user_input)
 
-    messages.append(update.message.text)
+    # Отправляем ответ пользователю
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-    context.bot.set_chat(update.effective_chat.id, {'messages': messages})
+# Создаем экземпляр Updater и добавляем обработчики команд и сообщений
+updater = Updater(token='YOUR_TOKEN', use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-# функция для запуска бота
-
-def main():
-
-    # создаем экземпляр Updater и передаем токен бота
-
-    updater = Updater(token='6288706593:AAFz6bQ4ErJiUG_9KuX1pn4QwDhWpc7p84Y', use_context=True)
-
-    # получаем объект диспетчера для регистрации обработчиков
-
-    dispatcher = updater.dispatcher
-
-    # регистрируем обработчики команд
-
-    start_handler = CommandHandler('start', start)
-
-    dispatcher.add_handler(start_handler)
-
-    # регистрируем обработчики сообщений
-
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-
-    dispatcher.add_handler(echo_handler)
-
-    # запускаем повторение сообщений
-
-    job_queue = updater.job_queue
-
-    job_queue.run_repeating(repeat_message, interval=random.randint(60, 1800), context=update.effective_chat.id)
-
-    # запускаем бота
-
-    updater.start_polling()
-
-    # останавливаем бота при нажатии Ctrl+C
-
-    updater.idle()
-
-if __name__ == '__main__':
-
-    main()
-
-    
+# Запускаем бота
+updater.start_polling()
+updater.idle()
